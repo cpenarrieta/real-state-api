6351f4cd6dd809b3f26b8af0ee845ebe6eca8cde
Cristian Penarrieta <cristian.penarrieta@gmail.com>
2020-08-04 22:44:23 -0700

    auth0 auth

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,10 @@ import { ApolloServer } from "apollo-server-express";
 import { typeDefs } from "./graphql/typeDefs";
 import { resolvers } from "./graphql/resolvers";
 import cors from "cors";
-import { createContext } from "./context";
+import { createContext, MyRequest } from "./context";
 import cookieParser from "cookie-parser";
+import jwt from 'express-jwt';
+import jwks from 'jwks-rsa';
 
 (async () => {
   const app = express();
@@ -18,24 +20,21 @@ import cookieParser from "cookie-parser";
   );
   app.use(cookieParser());
 
-  // app.post('/graphql', async (req: MyRequest, res, next) => {
-  //   const authorization = req.headers.authorization;
-  //   if (authorization) {
-  //     try { 
-  //       const token = authorization.split(" ")[1];
-  //       const payload = await firebase.auth().verifyIdToken(token);
+  const requireAuth = jwt({
+    secret: jwks.expressJwtSecret({
+      cache: true,
+      rateLimit: true,
+      jwksRequestsPerMinute: 5,
+      jwksUri: process.env.AUTH0_JWKS_URI as string
+    }),
+    audience: process.env.AUTH0_AUDIENCE,
+    issuer: process.env.AUTH0_JWKS_ISSUER,
+    algorithms: ['RS256']
+  });
 
-  //       req.user = {
-  //         userId: payload.uid,
-  //         email: payload.email,
-  //         role: payload.admin,
-  //       };
-  //     } catch (e) {
-  //       console.error("ERROR with verifyIdToken");
-  //     }
-  //   }
-  //   next();
-  // });
+  app.post('/graphql', requireAuth, async (req: MyRequest, res, next) => {
+    next();
+  });
 
   app.get("/", (_req, res) => res.send("hello"));
 
