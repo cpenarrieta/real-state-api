3a48ab1f271490a4ecb6020e3fe26e9d4c8aa33e
Cristian Penarrieta <cristian.penarrieta@gmail.com>
2020-10-28 23:26:03 -0700

    deploy script

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo What should the version be?
+read VERSION
+
+docker build -t cpenarrieta/realtor-app:$VERSION .
+docker push cpenarrieta/realtor-app:$VERSION
+ssh root@159.89.138.157 "docker pull cpenarrieta/realtor-app:$VERSION && docker tag cpenarrieta/realtor-app:$VERSION dokku/api:$VERSION && dokku deploy api $VERSION"
